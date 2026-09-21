5e93eef7e1232ba93e341c7e1d373c3a103b4e6a
MUJ660151 <Maximilliamu@gmail.com>
2026-09-20 22:48:47 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Math.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.control;
+
+public class Math {
+    public static Double MaxOf(Double minimum, Double input) {
+        if (minimum > input) return minimum;
+        else return input;
+    }
+
+    public static Double CoerceIn() {
+
+    }
+
+}
